eae780fc72429466e9f0a67451b793c41db5418e
Cmoy07 <cmoyer07@comcast.net>
2025-10-07 09:19:52 -0400

    Added starter code.

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("Hello World!");
+const sport = "Basketball";
+let team = "Sky";
+let jersey = 88;
+let hasWonChampionship = false;
+let stadiumLocation = null;
+let nextGame;
+console.log(team, jersey, hasWonChampionship, stadiumLocation, nextGame)
+let player = {
+  name: "Kahleah Copper",
+  position: "Guard",
+  pointsPerGame: 18.7
+};
+console.log(player.name); // Kahleah Copper
+player.pointsPerGame = 19.2;
+console.log(player.pointsPerGame); // 19.2
+player.isStarter = true
+player.numOfGames = 19
+
+console.log(player)
